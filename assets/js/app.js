f316070254910ddd8d73a7cd6c886e0c98c80059
ajcudd <ajcuddeback@gmail.com>
2020-12-16 13:50:16 -0500

    added gsap animations

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,69 @@
 let controller;
 let slideScene;
+let pageScene
 
 function animateSlides() {
-    //Init controller
+    //Init controller for scroll magic
     controller = new ScrollMagic.Controller();
     // Select things
     const sliders = document.querySelectorAll(".slide");
     const nav = document.querySelector('.nav-header');
     //Loop over each slide
-    sliders.forEach(slide => {
+    sliders.forEach((slide, index, slides) => {
         const revealImg = slide.querySelector(".reveal-img");
         const img = slide.querySelector("img");
         const revealText = slide.querySelector('.reveal-text');
 
         //GSAP
+        // make timeline
         const slideTl = gsap.timeline({
+            // default for timeline
             defaults: { duration: 1, ease: "power2.inOut" }
         });
+        // moving the cover div away
         slideTl.fromTo(revealImg, { x: '0%' }, { x: "100%" })
+        //shrinking the image (the image has a overflow hidden to hide the extra img when it grows) 
+        //and -=1 will let the animation start 1s sooner
         slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, '-=1')
+        // Reavealing text
         slideTl.fromTo(revealText, { x: '0%' }, { x: "100%" }, "-=0.75")
+        //moveing the nav elements
         slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, "-=0.5")
+
+        // Create Scene
+        slideScene = new ScrollMagic.Scene({
+            // the trigger element is going to be the slide(each slide because of foreach)
+            triggerElement: slide,
+            //where on the page the animation will trigger
+            triggerHook: 0.25,
+            //this reverse false object will make it to where the images will always stay there. 
+            reverse: false
+        })
+            // this tween will make the gsap animation (slideTl) only happen when the scroll magic gets triggered
+            .setTween(slideTl)
+            // indicators with custom name and colors
+            .addIndicators({ colorStart: 'white', colorTrigger: 'white', name: 'slide' })
+            //This will add to the controller
+            .addTo(controller)
+        // New Animation
+        const pageTl = gsap.timeline();
+        let nextSlide = slides.length - 1 === index ? 'end' : slide[index + 1];
+        pageTl.fromTo(nextSlide, { y: '0%' }, { y: "50%" })
+        pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 })
+        pageTl.fromTo(nextSlide, { y: '50%' }, { y: "0%" }, '-=0.5')
+        //create new scene
+        pageScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            // this duration will be based on the height of the slide itself(which is pretty much whole page)
+            duration: '100%',
+            triggerHook: 0
+        })
+            .addIndicators({ colorStart: 'white', colorTrigger: 'white', name: 'page', indent: 200 })
+            // This pin will pin the slide div to the screen while animation is running. Kind of like position fixed
+            //push followers will make the next item coming in come in right away
+            .setPin(slide, { pushFollowers: false })
+            .setTween(pageTl)
+            .addTo(controller)
     })
 }
 animateSlides()
